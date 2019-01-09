420d31e91b10213ecf3402c14a6a14c56d607c66
SnnGnc <sinan.gencoglu@hotmail.com>
2019-01-09 16:12:24 +0300

    Update Fibonacci-Number.py

--- Fibonacci-Number.py
+++ Fibonacci-Number.py
@@ -23,3 +23,17 @@ def fib(self, N):
         fn = f0 + f1
         return fn
         
+# Recursion version
+
+def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0:
+            return 0
+        
+        if N == 1:
+            return 1
+        
+        return self.fib(N-1) + self.fib(N-2)
