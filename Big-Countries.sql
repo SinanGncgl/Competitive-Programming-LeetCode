9f6b1109a8d4283e15ed66c8d0af144dbd843483
SnnGnc <sinan.gencoglu@hotmail.com>
2019-01-25 22:29:06 +0300

    update

--- Big-Countries.sql
+++ Big-Countries.sql
@@ -10,7 +10,6 @@ There is a table World
 | Angola          | Africa     | 1246700    | 20609294     | 100990000     |
 +-----------------+------------+------------+--------------+---------------+
 
-
 A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
 
 Write a SQL solution to output big countries' name, population and area.
